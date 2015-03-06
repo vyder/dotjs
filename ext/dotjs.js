1c3c28ff1939c07cfd9ccedfd704d4b9ba33ec2f
Vidur Murali <vidur.murali@gmail.com>
2015-03-06 13:10:51 +0530

    Some ajax error handling

--- ext/dotjs.js
+++ ext/dotjs.js
@@ -1,10 +1,17 @@
-$.ajax({
-  url: 'https://localhost:3131/'+location.hostname.replace(/^www\./,'')+'.js',
-  dataType: 'text',
-  success: function(d){
-    $(function(){ eval(d) })
-  },
-  error: function(){
-    console.log('no dotjs server found at localhost:3131')
-  }
-})
+var server = "https://localhost:3131/";
+var resourceURL = server + location.hostname.replace(/^www\./,'')+'.js';
+try {
+	$.ajax({
+	  url: server + location.hostname.replace(/^www\./,'')+'.js',
+	  dataType: 'text',
+	  success: function(d){
+	    $(function(){ eval(d) })
+	  },
+	  error: function(){
+	    console.log("Can't connect to server at: " + server);
+	    console.log("Try accepting the SSL certificate.");
+	  }
+	})
+} catch(e) {
+	console.log("Couldn't load dotjs resource: " + resourceURL);
+}
